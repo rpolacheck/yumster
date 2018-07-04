ae4b09bfb0c7cdaa4fa604c544df7601cda4e6b7
Bob Polacheck <rpolacheck@gmail.com>
2018-07-04 05:28:01 +0000

    bundle update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'bootstrap'
+require 'popper_js'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
