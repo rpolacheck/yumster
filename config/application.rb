44fd7289f9fb055fa84b4b8b390c60c96eb41c38
Bob Polacheck <rpolacheck@gmail.com>
2018-07-04 05:16:57 +0000

    Heroku trouble

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Yumster
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    confit.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
